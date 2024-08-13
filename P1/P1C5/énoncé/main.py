8d9d220030c23cea649143ad5844309d0b47e9a6
MooStrz <caraudyanis@hotmail.com>
2024-08-13 23:10:15 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,29 @@
-# Écrivez votre code ici !
+# Écrivez votre code ici !>>> taille = 1m70
+  File "<stdin>", line 1
+    taille = 1m70
+             ^
+SyntaxError: invalid decimal literal
+>>> taille "1m70"
+  File "<stdin>", line 1
+    taille "1m70"
+           ^^^^^^
+SyntaxError: invalid syntax
+>>> taille = "1m70"
+>>> est_etudiant = False
+>>> age
+22
+>>> taille
+'1m70'
+>>> est_etudiant
+False
+>>> type age
+  File "<stdin>", line 1
+    type age
+            ^
+SyntaxError: invalid syntax
+>>> type (age)
+<class 'int'>
+>>> type (taille)
+<class 'str'>
+>>> type (est_etudiant)
+<class 'bool'>
